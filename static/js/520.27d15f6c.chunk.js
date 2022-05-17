0200a671aec40a60e7ddfdc9492da55dacfb81a8
Agrratt <87565047+Agrratt@users.noreply.github.com>
2022-05-17 12:13:45 +0000

    Deploying to gh-pages from @ Agrratt/goit-react-hw-05-movies@5f5720e55615374f22aac93ef7f22633c67dc6e8 🚀

---
+++ static/js/520.27d15f6c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{4520:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(501),o=r(184);function u(){return(0,o.jsxs)("p",{children:["Sorry, page not found. Go to",(0,o.jsx)(n.rU,{to:"/",children:" home page"})]})}}}]);
+//# sourceMappingURL=520.27d15f6c.chunk.js.map
--- static/js/520.fe503f35.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{4520:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(3504),o=r(184);function u(){return(0,o.jsxs)("p",{children:["Sorry, page not found. Go to",(0,o.jsx)(n.rU,{to:"/",children:" home page"})]})}}}]);
-//# sourceMappingURL=520.fe503f35.chunk.js.map
